5aae221f80ffa3677fd3a9084c16d854ed4fc396
jameshsu1125 <jameshsu1125@gmail.com>
2024-05-28 19:40:35 +0800

    feat: Improve loading process and add lazy loading for images

--- src/components/loadingProcess/index.tsx
+++ src/components/loadingProcess/index.tsx
@@ -1,11 +1,9 @@
 import { Context } from '@/settings/constant';
 import { ActionType, IReactProps } from '@/settings/type';
-import { memo, useContext } from 'react';
+import { memo, useContext, useEffect } from 'react';
 import ReactLoading from 'react-loading';
 
-const Background = () => (
-  <div className='absolute top-0 h-full w-full bg-backgroundColor opacity-90' />
-);
+const Background = () => <div className='absolute top-0 h-full w-full bg-quinary opacity-90' />;
 
 const Text = ({ children }: IReactProps) => (
   <span className='relative text-textColor'>{children}</span>
@@ -14,8 +12,16 @@ const Text = ({ children }: IReactProps) => (
 const LoadingProcess = memo(() => {
   const [context] = useContext(Context);
   const data = context[ActionType.LoadingProcess];
+
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, []);
+
   return (
-    <div className='absolute top-0 z-50 flex h-full w-full flex-col items-center justify-center space-y-3'>
+    <div className='fixed top-0 z-50 flex h-full w-full flex-col items-center justify-center space-y-3'>
       <Background />
       <ReactLoading className='relative' type={data?.type} />
       {data?.body && <Text>{data.body}</Text>}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,16 +1,20 @@
-import { memo, useEffect, useState } from 'react';
+import { Context } from '@/settings/constant';
+import OnloadProvider from 'lesca-react-onload';
+import { memo, useContext, useEffect, useState } from 'react';
 import { HomeContext, HomeState, HomeStepType, THomeState } from './config';
+import Content from './content';
 import './index.less';
 import Landing from './landing';
-import OnloadProvider from 'lesca-react-onload';
-import Content from './content';
+import { ActionType } from '@/settings/type';
 
 const Home = memo(() => {
+  const [, setContext] = useContext(Context);
   const [state, setState] = useState<THomeState>(HomeState);
 
   useEffect(() => {
     if (state.step >= HomeStepType.loaded) {
       // all content loaded
+      setContext({ type: ActionType.LoadingProcess, state: { enabled: false } });
     }
   }, [state.step]);
 
