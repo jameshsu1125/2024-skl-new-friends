3fd406b734b16ed977eec68fb5a58570efb918ee
jameshsu1125 <jameshsu1125@gmail.com>
2024-05-30 19:01:26 +0800

    feat: Update page layout and optimize image loading

--- src/components/page/index.tsx
+++ src/components/page/index.tsx
@@ -1,12 +1,11 @@
 import { IReactProps } from '@/settings/type';
 import { memo } from 'react';
-import Div100vh from 'react-div-100vh';
 
 const Page = memo(({ children }: IReactProps) => (
   <div className='relative w-full'>
-    <Div100vh className='min-h-[1000px] min-w-[750px] pt-28 lg:min-h-[700px]'>
+    <div className='h-[930px] min-w-[750px] pt-28 lg:h-[650px]'>
       <div className='relative h-full w-full'>{children}</div>
-    </Div100vh>
+    </div>
   </div>
 ));
 export default Page;
--- src/pages/home/landing/index.tsx
+++ src/pages/home/landing/index.tsx
@@ -5,26 +5,15 @@ import { memo, useContext, useMemo } from 'react';
 import { HomeContext, HomeStepType } from '../config';
 import { twMerge } from 'tailwind-merge';
 import './index.less';
-
-const SIZE = {
-  lg: [
-    7.575057736720554, 8.535114583333334, 6.332052083333334, 5.14028125, 4.797192708333333,
-    6.846609375, 5.940854166666667, 9.650286458333333, 9.332875, 7.522619791666667,
-    7.1019322916666665, 9.634020833333333, 8.8284375, 7.190234375,
-  ],
-  md: [
-    16.085851851851853, 13.957796296296296, 22.672981481481482, 21.927222222222223,
-    17.674092592592594, 16.685703703703705,
-  ],
-};
+import './char.less';
 
 const Landing = memo(() => {
   const [device] = useMedia();
   const [, setState] = useContext(HomeContext);
 
   const size = useMemo(() => {
-    if (device >= MediaType.MD) return SIZE.lg;
-    return SIZE.md;
+    if (device >= MediaType.MD) return 15;
+    return 6;
   }, [device]);
 
   return (
@@ -44,11 +33,14 @@ const Landing = memo(() => {
           </div>
           <div className='pointer-events-none absolute left-0 top-2 h-full w-full overflow-hidden'>
             <div className={twMerge(device >= MediaType.MD ? 'ground' : 'ground-m')}>
-              {size.map((size, index) => (
+              {[...new Array(size).keys()].map((index) => (
+                <div className={`c${index}`} key={`c${index}`} />
+              ))}
+              {/* {size.map((size, index) => (
                 <div key={`character${index}`} style={{ width: `${size}%` }}>
                   <div className={`character${index}`} />
                 </div>
-              ))}
+              ))} */}
             </div>
           </div>
         </div>
